ad47a93a59f39c5b8aa8380c84ce9ec901c0faea
navctns <navsoft18@gmail.com>
2026-08-11 15:54:20 +0530

    blog thumbnail image updated

---
+++ blogs/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const year = document.getElementById("year");
+
+  if (year) {
+    year.textContent = new Date().getFullYear();
+  }
+});
