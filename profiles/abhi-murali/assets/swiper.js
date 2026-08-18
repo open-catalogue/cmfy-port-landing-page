c606458c9563cb3c66a4fe2dc5dfd19ecd58517c
navctns <navsoft18@gmail.com>
2026-08-18 16:25:52 +0530

    abhi murali's profile added

---
+++ profiles/abhi-murali/assets/swiper.js
@@ -0,0 +1,261 @@
+const swiper = new Swiper(".projectsSwiper", {
+
+    /*
+     * Default: Mobile
+     * One slide visible
+     */
+    slidesPerView: 1,
+
+    /*
+     * Move only ONE item at a time
+     */
+    slidesPerGroup: 1,
+
+    /*
+     * Space between slides
+     */
+    spaceBetween: 15,
+
+
+    /*
+     * Navigation arrows
+     */
+    navigation: {
+
+      nextEl: ".swiper-button-next",
+
+      prevEl: ".swiper-button-prev"
+
+    },
+
+
+    /*
+     * Pagination dots
+     */
+    pagination: {
+
+      el: ".swiper-pagination",
+
+      clickable: true
+
+    },
+
+
+    /*
+     * Touch/swipe is enabled by default
+     */
+
+
+    /*
+     * Responsive settings
+     */
+    breakpoints: {
+
+      /*
+       * Tablet
+       * 768px and above
+       */
+      768: {
+
+        slidesPerView: 2,
+
+        slidesPerGroup: 1,
+
+        spaceBetween: 20
+
+      },
+
+
+      /*
+       * Large screens
+       * 992px and above
+       */
+      992: {
+
+        slidesPerView: 3,
+
+        slidesPerGroup: 1,
+
+        spaceBetween: 20
+
+      }
+
+    }
+
+  });
+
+//   wall paintings swiper
+
+const wallPaintingsSwiper = new Swiper(".wallPaintingsSwiper", {
+
+    /*
+     * Default: Mobile
+     * One slide visible
+     */
+    slidesPerView: 1,
+
+    /*
+     * Move only ONE item at a time
+     */
+    slidesPerGroup: 1,
+
+    /*
+     * Space between slides
+     */
+    spaceBetween: 15,
+
+
+    /*
+     * Navigation arrows
+     */
+    navigation: {
+
+      nextEl: ".swiper-button-next",
+
+      prevEl: ".swiper-button-prev"
+
+    },
+
+
+    /*
+     * Pagination dots
+     */
+    pagination: {
+
+      el: ".swiper-pagination",
+
+      clickable: true
+
+    },
+
+
+    /*
+     * Touch/swipe is enabled by default
+     */
+
+
+    /*
+     * Responsive settings
+     */
+    breakpoints: {
+
+      /*
+       * Tablet
+       * 768px and above
+       */
+      768: {
+
+        slidesPerView: 2,
+
+        slidesPerGroup: 1,
+
+        spaceBetween: 20
+
+      },
+
+
+      /*
+       * Large screens
+       * 992px and above
+       */
+      992: {
+
+        slidesPerView: 3,
+
+        slidesPerGroup: 1,
+
+        spaceBetween: 20
+
+      }
+
+    }
+
+  });
+
+  //   crafts swiper
+
+const craftsSwiper = new Swiper(".craftsSwiper", {
+
+    /*
+     * Default: Mobile
+     * One slide visible
+     */
+    slidesPerView: 1,
+
+    /*
+     * Move only ONE item at a time
+     */
+    slidesPerGroup: 1,
+
+    /*
+     * Space between slides
+     */
+    spaceBetween: 15,
+
+
+    /*
+     * Navigation arrows
+     */
+    navigation: {
+
+      nextEl: ".swiper-button-next",
+
+      prevEl: ".swiper-button-prev"
+
+    },
+
+
+    /*
+     * Pagination dots
+     */
+    pagination: {
+
+      el: ".swiper-pagination",
+
+      clickable: true
+
+    },
+
+
+    /*
+     * Touch/swipe is enabled by default
+     */
+
+
+    /*
+     * Responsive settings
+     */
+    breakpoints: {
+
+      /*
+       * Tablet
+       * 768px and above
+       */
+      768: {
+
+        slidesPerView: 2,
+
+        slidesPerGroup: 1,
+
+        spaceBetween: 20
+
+      },
+
+
+      /*
+       * Large screens
+       * 992px and above
+       */
+      992: {
+
+        slidesPerView: 3,
+
+        slidesPerGroup: 1,
+
+        spaceBetween: 20
+
+      }
+
+    }
+
+  });
